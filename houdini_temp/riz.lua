df88bfec40c702085c1773d38116147354f1f545
YingQ <hqq.2020@foxmail.com>
2022-03-07 15:18:23 +0800

    WFC

--- houdini_temp/riz.lua
+++ houdini_temp/riz.lua
@@ -1,10 +1,6 @@
 
-ZomLoad({File={Path="C:/Users/YingQ/sidefx_rizom_bridge_untitled_rizomuv_unwrap1.fbx", ImportGroups=true, XYZ=true}, NormalizeUVW=true})
+ZomLoad({File={Path="C:/Users/YingQ/sidefx_rizom_bridge_untitled_rizomuv_optimize1.fbx", ImportGroups=true, XYZUVW=true, UVWProps=true}, NormalizeUVW=true})
 
-ZomSelect({PrimType="Edge", Select=true, ResetBefore=true, ProtectMapName="Protect", FilterIslandVisible=true, Auto={Skeleton={Open=true}, PipesCutter=false, HandleCutter=true}})
-ZomCut({PrimType="Edge"})
-ZomUnfold({PrimType="Edge", MinAngle=1e-005, Mix=1, Iterations=1, PreIterations=5, StopIfOutOFDomain=false, RoomSpace=0, PinMapName="Pin", ProcessNonFlats=true, ProcessSelection=true, ProcessAllIfNoneSelected=true, ProcessJustCut=true, BorderIntersections=true, TriangleFlips=true})
-ZomIslandGroups({Mode="DistributeInTilesEvenly", MergingPolicy=8322, GroupPath="RootGroup"})ZomPack({ProcessTileSelection=false, RecursionDepth=1, RootGroup="RootGroup", Scaling={Mode=2}, Rotate={}, Translate=true, LayoutScalingMode=2})
-
-ZomSave({File={Path="C:/Users/YingQ/sidefx_rizom_bridge_untitled_rizomuv_unwrap1.fbx", UVWProps=true}, __UpdateUIObjFileName=true})
+ZomOptimize({PrimType="Edge", WorkingSet="Visible&Flat", Iterations=1, Mix=1.0, AngleDistanceMix=1.0, RoomSpace=0, MinAngle=1e-05, PinMapName="Pin", FillHoles=0, BorderIntersections=0, TriangleFlips=0})
+ZomSave({File={Path="C:/Users/YingQ/sidefx_rizom_bridge_untitled_rizomuv_optimize1.fbx", UVWProps=true}, __UpdateUIObjFileName=true})
 ZomQuit()
