d9f346732dd5222a2961997319ddd585a9fca86b
YingQ <hqq.2020@foxmail.com>
2022-03-18 16:19:49 +0800

    action button

---
+++ houdini_temp/hfs53732-27.tmp.py
@@ -0,0 +1,25 @@
+﻿# current node
+node = kwargs["node"]
+# print(kwargs)
+# current parameter
+pt = kwargs["parmtuple"]
+
+# its template
+template = pt.parmTemplate()
+
+# parameter's tags
+tags = template.tags()
+print(tags)
+# invert collapsed tag
+tags['editor'] = str(1-int(tags['editor']))
+
+# invert icon
+tags['script_action_icon'] = "KEYS_Up" if tags['script_action_icon'] == "KEYS_Down" else "KEYS_Down"
+
+# set updated tags
+template.setTags(tags)
+
+# replace parameter template with updated tags
+group = node.parmTemplateGroup()
+group.replace(pt.name(), template)
+node.setParmTemplateGroup(group)
